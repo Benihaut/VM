d2babd5829d8e9c6a08e1059e8b94e342f4ac54c
béniHaut bényo <ulrich.parfait@gmail.com>
2025-02-15 22:10:10 +0100

    Ajout du dossier [sauvegarde]

--- sauvegarde/projetcda/start.sh
+++ sauvegarde/projetcda/start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Démarrage de PHP-FPM
 php-fpm &
-nginx -g 'daemon off;'
+
+# Démarrage de Nginx
+nginx -g "daemon off;" &
+
+# Attente infinie pour garder le conteneur en cours d'exécution
+wait
 
